344ddc0f0b7b7a622b42e3405906cce37b0c1525
6ex06en <kerzo@mail.ru>
2015-09-09 16:51:24 +0300

    validation and method User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,22 @@
 class User < ActiveRecord::Base
+	validates :name, length: { minimum: 3, maximum: 20 }
+	validates :email, presence: true, confirmation: true, uniqueness: { case_sensitive: false }
+	validates :password, length: {minimum: 8}, on: :create
+	validates :password_confirmation, presence: true, on: :create
+	has_secure_password
+	before_create :create_auth_token 
+
+def User.new_token
+	SecureRandom.urlsafe_base64
+end
+
+def User.encrypt(token)
+	Digest::SHA1.hexdigest(token.to_s)
+end
+
+private
+
+def create_auth_token
+	self.auth_token = User.encrypt(User.new_token)
+end
 end
