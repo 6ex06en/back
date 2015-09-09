9131f9a68a2acf1eecc2715bcc419a7dd7fb1979
Kerz <kerzo@mail.ru>
2015-09-09 22:09:08 +0300

    tests model specs

---
+++ app/models/test.rb
@@ -0,0 +1,22 @@
+class Test < ActiveRecord::Base
+	serialize :answers, Array
+	validates :question, presence:true, length: {maximum: 800, minimum: 3}, uniqueness: { case_sensitive: false }
+	validates :typeinput, presence:true,length: {maximum: 20, minimum: 5}, inclusion: {in: %w(textInput checkboxInput radioButton)}
+	validates :score, presence:true, length: {minimum: 1}, numericality: { greater_than: 0 }
+	validates :answers, presence:true, :if => Proc.new{|x| x.typeinput == "checkboxInput"}
+	before_create :create_checksum
+
+	private
+
+	def create_checksum
+		rand = rand(1000000..2000000)
+		loop do 
+			if Test.find_by_checksum(rand)
+				rand = rand(1000000..2000000)
+			else
+				break
+			end
+		end
+		self.checksum = rand 
+	end 
+end
---
+++ spec/models/test_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Test, type: :model do
+
+	it{is_expected.to respond_to(:question)}
+	it{is_expected.to respond_to(:score)}
+	it{is_expected.to respond_to(:checksum)}
+	it{is_expected.to respond_to(:answers)}
+	it{is_expected.to respond_to(:typeinput)}
+
+	it "before_create #create_checksum" do
+		test = Test.create(question: "1+1", score: 1, typeinput: "radioButton")
+		expect(test).to be_valid
+		expect(test.checksum).to be_between(1000000, 2000000)
+	end
+end
