57008ae0e29c2352996469b55ad1fed07b5cfecd
Kerz <kerzo@mail.ru>
2015-09-10 00:50:18 +0300

    add factory test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,19 @@
 FactoryGirl.define do
+
+  factory :test do
+    question "1+1"
+    score 100
+    typeinput "radioButton"
+    answers [1,2,3]
+  end
+
   factory :user do
     name "Test_user"
     password "qweqweqwe"
     password_confirmation "qweqweqwe"
     email "test_admin@test.ru"
     admin false
+
     factory :admin do
     	name "Test_admin"
    		email "test_admin@test.ru"
