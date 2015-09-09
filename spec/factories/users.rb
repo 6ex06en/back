392b33c295dc8fb0af08abe7d19e52e4950b58e1
6ex06en <kerzo@mail.ru>
2015-09-09 16:50:59 +0300

    add spec user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,17 @@
 FactoryGirl.define do
   factory :user do
-    
+    name "Test_user"
+    password "qweqweqwe"
+    password_confirmation "qweqweqwe"
+    email "test_admin@test.ru"
+    admin false
+    factory :admin do
+    	name "Test_admin"
+   		email "test_admin@test.ru"
+   		password "qweqweqwe"
+    	password_confirmation "qweqweqwe"
+   		admin true
+    end
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it{is_expected.to respond_to(:name)}
+  it{is_expected.to respond_to(:email)}
+  it{is_expected.to respond_to(:password)}
+  it{is_expected.to respond_to(:auth_token)}
+  let(:admin) { FactoryGirl.create(:admin)}
+
+  it "new user should be admin" do
+  	expect(admin.admin).to be_truthy
+  end
 end
